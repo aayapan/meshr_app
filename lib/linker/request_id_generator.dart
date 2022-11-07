5d005a5c7bd045add7296acbeeebe278d906aae0
jkcrts <jk.crtes@gmail.com>
2022-11-08 01:05:39 +0800

    request id update

--- lib/linker/request_id_generator.dart
+++ lib/linker/request_id_generator.dart
@@ -15,11 +15,8 @@ class RequestID {
   RequestID._internal();
 
   static String create(String user){
-    DateTime datetime = DateTime.now();
-    String date = DateTime(datetime.year, datetime.month, datetime.day).toString().replaceAll("00:00:00.000", "");
     var rqid = utf8.encode(user);
     String sha1Version = sha1.convert(rqid).toString();
-    sha1Version += "_"  + count.toString() + "_" + date;
     print(sha1Version);
     count++;
     return sha1Version;
